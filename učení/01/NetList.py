d966e2a33d5eaa4c3a81eb40b9f7a6bbd38fcbfb
Filip Maroul <filip@filipnet.cz>
2023-04-14 13:26:40 +0200

    Uceni

---
+++ učení/01/NetList.py
@@ -0,0 +1,15 @@
+import netifaces
+
+# Získání seznamu síťových rozhraní
+interfaces = netifaces.interfaces()
+
+# Pro každé síťové rozhraní získáme IP adresy
+for iface in interfaces:
+    addresses = netifaces.ifaddresses(iface)
+    ip_addresses = addresses.get(netifaces.AF_INET)
+    
+    # Pokud jsou k dispozici nějaké IP adresy, vypište je
+    if ip_addresses:
+        print(f"Síťové rozhraní {iface}:")
+        for ip in ip_addresses:
+            print(f"- IP adresa: {ip['addr']}")
